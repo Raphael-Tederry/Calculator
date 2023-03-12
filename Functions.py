8a0a4fdd8f1780b6c8bab6accc85f1718606e292
BAR <bartubul826@gmail.com>
2023-03-12 22:51:44 +0200

    Fixed.

--- Functions.py
+++ Functions.py
@@ -3,12 +3,13 @@ import math
 #functions for the Calculator:
 
 def multiply(str_x, str_y):
-    if str_x.isnumeric() and str_y.isnumeric():
-        sum = int(str_x) * int(str_y)
-        return str(sum)
+    if str_x.isdigit() and str_y.isdigit():
+        result = int(str_x) * int(str_y)
+        return str(result) if isinstance(result, int) else str(round(result, 6))
     else:
         try:
-            return str(float(str_x) * float(str_y))
+            result = float(str_x) * float(str_y)
+            return str(int(result)) if isinstance(result, int) else str(round(result, 6))
         except ValueError:
             return "Math Error"
 
@@ -16,58 +17,65 @@ def multiply(str_x, str_y):
 def divide(str_x, str_y):
 
     if str_x.isnumeric() and str_y.isnumeric() and int(str_y) != 0 :
-         sum = int(str_x) / int(str_y)
-         return str(sum)
+        result = int(str_x) / int(str_y)
+        return str(result) if isinstance(result, int) else str(round(result, 6))
     else:
         try:
             if float(str_y) != 0:
-             return str(float(str_x) / float(str_y))
-            else:
-                return 'Math Error'
+                result = float(str_x) / float(str_y)
+                return str(int(result)) if isinstance(result, int) else str(round(result, 6))
         except ValueError:
             return 'Math Error'
 
 
-
-
 def plus(str_x, str_y):
-    if str_x.isnumeric() and str_y.isnumeric():
-        sum = int(str_x) + int(str_y)
-        return str(sum)
+    if str_x.isdigit() and str_y.isdigit():
+        result = int(str_x) + int(str_y)
+        return str(result) if isinstance(result, int) else str(round(result, 6))
     else:
         try:
-            return str(float(str_x) + float(str_y))
-        except ValueError:
-         return "Math Error"
-
-
-def minus(str_x, str_y):
-    if str_x.isnumeric() and str_y.isnumeric():
-        sum = int(str_x) - int(str_y)
-        return str(sum)
-    else:
-        try:
-            return str(float(str_x) - float(str_y))
+            result = float(str_x) + float(str_y)
+            return str(int(result)) if isinstance(result, int) else str(round(result, 6))
         except ValueError:
             return "Math Error"
 
 
-def exponent(str_x, str_y):
-    if str_x.isnumeric() and str_y.isnumeric():
-        return str(math.pow(int(str_x), int(str_y)))
+def minus(str_x, str_y):
+    if str_x.isdigit() and str_y.isdigit():
+        result = int(str_x) - int(str_y)
+        return str(result) if isinstance(result, int) else str(round(result, 6))
     else:
         try:
-            return str(math.pow(float(str_x), float(str_y)))
+            result = float(str_x) - float(str_y)
+            return str(int(result)) if isinstance(result, int) else str(round(result, 6))
+        except ValueError:
+            return "Math Error"
+
+
+
+def exponent(str_x, str_y):
+    if str_x.isdigit() and str_y.isdigit():
+        sum = math.pow(int(str_x), int(str_y))
+        return str(int(sum)) if sum.is_integer() else str(sum)
+    else:
+        try:
+            sum1 = math.pow(float(str_x), int(str_y))
+            return str(int(sum1)) if sum1.is_integer() else str(sum1)
         except ValueError:
             return "Math Error"
 
 def root(str_x):
-    if str_x.isnumeric() and int(str_x) >= 0:
-        return str(math.sqrt(int(str_x)))
-    elif isfloat(str_x) and float(str_x) >=0:
-        return str(math.sqrt(float(str_x)))
+    if str_x.isdigit() and int(str_x) >= 0:
+        sum = math.sqrt(int(str_x))
+        return str(int(sum)) if sum.is_integer() else str(sum)
     else:
-        return 'Math Error'
+        try:
+           sum1 = math.sqrt(float(str_x))
+           return str(int(sum1)) if sum1.is_integer() else str(sum1)
+        except ValueError:
+            return "Math Error"
+
+
 
 
 
@@ -84,7 +92,7 @@ def isfloat(num):
 
 if __name__ == "__main__":
 
-    r = multiply('0.9', '0.1')
+    r = minus('5.2', '2')
     print(r)
 
 
