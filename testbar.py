3294107bbe33dfffd453c5a85739aa51d7951564
Raphael <rptksmil@gmail.com>
2023-03-13 00:33:10 +0200

    bars homework

--- testbar.py
+++ testbar.py
@@ -1,18 +1,12 @@
 import math
-def eval(equation):
-    return equation.split()
 
 
-def math_stuff(snum1, snum2):
-    match snum1:
-        case 1: pass
+def reform_paragaph(sentense):
+    pass
 
 
 if __name__ == '__main__':
     # math_stuff('09+88')
-    d = 'a= 12345'
+    sentense = "Mitzee was in prison for a crime he did not commit.Well, OK, he probably had committed it once or twice.Who knew? Who kept count? But he was very sure that he had been incarcerated now because of something he didn’t do. He glared through the prison bars, plotting. He was Houdini, Al Capone, and Einstein rolled into one. There was nothing he couldn’t do… except break out of this prison, apparently."
 
-
-    print(d[3:])
-
-
+    print()
