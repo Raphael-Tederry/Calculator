80f31025723fdc8dee04af02174a68fe56d6872c
Raphael <rptksmil@gmail.com>
2023-03-13 00:37:24 +0200

    bars homework

--- testbar.py
+++ testbar.py
@@ -1,12 +1,18 @@
 import math
 
 
-def reform_paragaph(sentense):
+def reform_paragraph(sentence):
+    """
+    this function will receive a sentence(how will contain a full paragraph) and will add the missing lines
+     everywhere there is a dot.
+    :param sentence: a string of one sentence
+    :return: the full paragraph as a string
+    """
     pass
 
 
 if __name__ == '__main__':
     # math_stuff('09+88')
-    sentense = "Mitzee was in prison for a crime he did not commit.Well, OK, he probably had committed it once or twice.Who knew? Who kept count? But he was very sure that he had been incarcerated now because of something he didn’t do. He glared through the prison bars, plotting. He was Houdini, Al Capone, and Einstein rolled into one. There was nothing he couldn’t do… except break out of this prison, apparently."
+    sentence = "Mitzee was in prison for a crime he did not commit.Well, OK, he probably had committed it once or twice.Who knew? Who kept count? But he was very sure that he had been incarcerated now because of something he didn’t do. He glared through the prison bars, plotting. He was Houdini, Al Capone, and Einstein rolled into one. There was nothing he couldn’t do… except break out of this prison, apparently."
 
     print()
