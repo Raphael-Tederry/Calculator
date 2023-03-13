f1d8a97b41a2ef4ef43880aeac4679dba4a74e82
Raphael <rptksmil@gmail.com>
2023-03-13 21:22:48 +0200

    you know

--- Functions.py
+++ Functions.py
@@ -29,15 +29,12 @@ def divide(str_x, str_y):
 
 
 def plus(str_x, str_y):
-    if str_x.isdigit() and str_y.isdigit():
-        result = int(str_x) + int(str_y)
-        return str(result) if isinstance(result, int) else str(round(result, 6))
-    else:
-        try:
-            result = float(str_x) + float(str_y)
-            return str(int(result)) if isinstance(result, int) else str(round(result, 6))
-        except ValueError:
-            return "Math Error"
+    try:
+        result = float(str_x) + float(str_y)
+        # 2.2  or  2.0   2        0.0  or 0.2
+        return str(int(result)) if result - int(result) == 0 else str(round(result, 6))
+    except ValueError:
+        return "Math Error"
 
 
 def minus(str_x, str_y):
@@ -92,7 +89,7 @@ def isfloat(num):
 
 if __name__ == "__main__":
 
-    r = minus('5.2', '2')
+    r = plus('5a2', '2')
     print(r)
 
 
