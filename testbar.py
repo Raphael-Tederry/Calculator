c08c6d8e389dcbb0fff8503050c449ca7844d115
Raphael <rptksmil@gmail.com>
2023-03-13 19:06:56 +0200

    prepared stuff for the call

--- testbar.py
+++ testbar.py
@@ -1,4 +1,6 @@
-import math
+import Functions as f
+
+d = {"/": f.divide, "*": f.multiply, "-": f.minus, "+": f.plus}
 
 
 def reform_paragraph(sentence):
@@ -8,11 +10,35 @@ def reform_paragraph(sentence):
     :param sentence: a string of one sentence
     :return: the full paragraph as a string
     """
+
     pass
+
+
+def my_eval(str1):
+    for operator, func in d.items():
+        if operator in str1:
+            a, b = str1.split(operator)
+            print(func(a, b))
+    return eval(str1)
 
 
 if __name__ == '__main__':
     # math_stuff('09+88')
-    sentence = "Mitzee was in prison for a crime he did not commit.Well, OK, he probably had committed it once or twice.Who knew? Who kept count? But he was very sure that he had been incarcerated now because of something he didn’t do. He glared through the prison bars, plotting. He was Houdini, Al Capone, and Einstein rolled into one. There was nothing he couldn’t do… except break out of this prison, apparently."
+    sentence = "Mitzee was in prison for a crime he did not commit.Well, OK, he probably had committed it once or " \
+               "twice.Who knew? Who kept count? But he was very sure that he had been incarcerated now because of " \
+               "something he didn’t do. He glared through the prison bars, plotting. He was Houdini, Al Capone, " \
+               "and Einstein rolled into one. There was nothing he couldn’t do… except break out of this prison, " \
+               "apparently. "
 
-    print()
+    reform_paragraph(sentence)
+
+
+
+"""
+    s = ''
+    for c in sentence:
+        s += c if c != '.' else c + '\n'
+    print(s == sentence.replace('.', '.\n'))
+    print(s == sentence.replace('.', '.\n'))
+    print(s == sentence.replace('.', '.\n'))
+"""
